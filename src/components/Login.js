cc57c5427ddcaf9a2b4135e54fef6d3a6356ebef
Christopher Grady <grittygrady@gmail.com>
2020-11-25 12:12:08 -0800

    Updated css, added login template

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-import '../App.css'
+import './Login.css'
 
 class Login extends Component {
   render() {
     return (
       <div className='Login'>
-        <h3>TODO make a login form</h3>
+        <form>
+          <label htmlFor="email">Email: </label>
+          <input type="text" name="email" id="email" aria-label='email' required />
+          <br/>
+          <label htmlFor="password">Password: </label>
+          <input type="password" name="password" id="password" aria-label='password' required />
+          <button>Log In</button>
+        </form>
       </div>
     )
   }
