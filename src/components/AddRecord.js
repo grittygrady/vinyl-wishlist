d5121dbe933c78718f85d86d5ec038e9979e8005
Christopher Grady <grittygrady@gmail.com>
2020-11-30 14:25:28 -0800

    Added fetch to AddRecord

--- src/components/AddRecord.js
+++ src/components/AddRecord.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { v4 as uuidv4} from 'uuid'
+import config from '../config'
 import './AddRecord.css'
 
 class AddRecord extends Component {
@@ -15,10 +16,30 @@ class AddRecord extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault()
-    this.props.addNewRecord({ ...this.state, id: uuidv4() })
-    this.setState({
-      title: ''
+    const newRecord = {
+      title: this.state.title, id: uuidv4()
+    }
+    fetch(`${config.API_ENDPOINT}/recordslist`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newRecord)
     })
+      .then(res => {
+        if (!res.ok)
+          return res.json().then(e => Promise.reject(e))
+        return res.json()
+      })
+      .then(record => {
+        this.props.addNewRecord(record)
+        this.setState({
+          title: ''
+        })
+      })
+      .catch(error => {
+        console.error({ error })
+      })
   }
 
   render() {
