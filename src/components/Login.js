83cfed5db41663fb11491f5142722a7f1c1b4f6e
Christopher Grady <grittygrady@gmail.com>
2020-12-08 09:40:38 -0800

    initial commit, attempting express sessions

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ import './Login.css'
 
 class Login extends Component {
   state = {
-    email: '',
+    username: '',
     password: ''
   }
 
@@ -17,7 +17,7 @@ class Login extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     const userLogin = {
-      email: this.state.email,
+      username: this.state.username,
       password: this.state.password
     }
 
@@ -43,8 +43,8 @@ class Login extends Component {
     return (
       <div className='Login'>
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="email">Email: </label>
-          <input type="text" name="email" id="email" value={this.state.email} onChange={this.handleChange} aria-label='email' required />
+          <label htmlFor="username">username: </label>
+          <input type="text" name="username" id="username" value={this.state.username} onChange={this.handleChange} aria-label='username' required />
           <label htmlFor="password">Password: </label>
           <input type="password" name="password" id="password" value={this.state.password} onChange={this.handleChange} aria-label='password' required />
           <button>Log In</button>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,37 +1,103 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import config from '../config'
 import './Signup.css'
 
-class Signup extends Component {
-  state = {
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '' 
-  }
+const Signup = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+      confirm_password: ''
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .min(4, 'Minimum 4 characters')
+        .max(15, 'Maximum 15 characters')
+        .required('Required'),
+      email: Yup.string()
+        .email('Invalid email format')
+        .required('Required'),
+      password: Yup.string()
+        .min(8, 'Minimum 8 characters')
+        .required('Required'),
+      confirm_password: Yup.string()
+        .oneOf([Yup.ref('password')], 'Passwords do not match')
+        .required('Required')
+    }),
+    onSubmit: values => {
+      fetch(`${config.API_ENDPOINT}/user`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(values)
+      })
+      .then(res => {
+        if (!res.ok)
+          return res.json().then(e => Promise.reject(e))
+        return res.json()
+      })
+      .catch(error => {
+        console.error(error)
+      })
+      console.log(values)
+    }
+  })
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name] : e.target.value
-    })
-  }
-
-  render() {
     return (
       <div className='Signup'>
-        <form>
+        <form onSubmit={formik.handleSubmit}>
           <label htmlFor="username">Username: </label>
-          <input type="text" name="username" id="username" value={this.state.username} onChange={this.handleChange} aria-label='username' required />
+          <input 
+            type="text" 
+            name="username" 
+            value={formik.values.username}
+            onChange={formik.handleChange} 
+            aria-label='username' 
+          />
+          {formik.errors.username && formik.touched.username && (
+            <p>{formik.errors.username}</p>
+          )}
           <label htmlFor="email">Email: </label>
-          <input type="text" name="email" id="email" value={this.state.email} onChange={this.handleChange} aria-label='email' required />
+          <input 
+            type="text" 
+            name="email" 
+            value={formik.values.email} 
+            onChange={formik.handleChange} 
+            aria-label='email'    
+          />
+          {formik.errors.email && formik.touched.email && (
+            <p>{formik.errors.email}</p>
+          )}
           <label htmlFor="password">Password: </label>
-          <input type="password" name="password" id="password" value={this.state.password} onChange={this.handleChange} aria-label='password' required />
-          <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input type="password" name="confirmPassword" id="confirmPassword" value={this.state.confirmPassword} onChange={this.handleChange} aria-label='confirm password' required />
-          <button>Log In</button>
+          <input 
+            type="password" 
+            name="password" 
+            value={formik.values.password} 
+            onChange={formik.handleChange} 
+            aria-label='password' 
+          />
+          {formik.errors.password && formik.touched.password && (
+            <p>{formik.errors.password}</p>
+          )}
+          <label htmlFor="confirm_password">Confirm Password:</label>
+          <input 
+            type="password" 
+            name="confirm_password"
+            value={formik.values.confirm_password} 
+            onChange={formik.handleChange} 
+            aria-label='confirm password'
+          />
+          {formik.errors.confirm_password && formik.touched.confirm_password && (
+            <p>{formik.errors.confirm_password}</p>
+          )}
+          <button type='submit'>Sign Up</button>
         </form>
       </div>
     )
   }
-}
 
 export default Signup
