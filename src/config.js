f720e62243127d5a57e1f799d766529c610e0ebd
Christopher Grady <grittygrady@gmail.com>
2020-12-16 11:53:17 -0800

    HOPE

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+const inDev = process.env.NODE_ENV === 'development'
+
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api'
+  API_ENDPOINT: inDev ? 'http://localhost:8000/api' : 'https://lit-mesa-39960.herokuapp.com/api'
 }
