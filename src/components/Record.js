fa14068f3bc5536ced26da223846a3cbb829581e
Christopher Grady <grittygrady@gmail.com>
2020-12-18 07:53:12 -0800

    updating patch

--- src/components/Record.js
+++ src/components/Record.js
@@ -54,6 +54,7 @@ class Record extends Component {
     .then((res) => {
       console.log(res)
       if (!res.ok) return res.json().then((e) => Promise.reject(e));
+      res.json()
     })
       .then(record => {
         this.props.updateRecord(record)
