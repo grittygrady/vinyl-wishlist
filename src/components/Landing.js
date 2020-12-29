fb7525ee7f61bdc0d94f4dbe96ce33763b63c535
Christopher Grady <grittygrady@gmail.com>
2020-12-29 08:41:40 -0800

    Added demo login credentials

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,6 +7,10 @@ class Landing extends Component {
       <div className='Landing'>
         <h3>Never forget the records you've been lusting after!</h3>
         <p className='landing-description'>Too many times we find ourselves at the record store, and when we try to recall the vinyl we've been wanting, we draw a blank. Sign up and start adding records, and get vinyl hunting!</p>
+        <br />
+        <hr></hr>
+        <br />
+        <p>Try logging in with the username "demo" and the password "password" to give it a try!</p>
       </div>
     )
   }
